d9e0a734680417846ffaf59a87f10a4604f26442
Shiv Pratap Singh <pratapsinghshiv898@gmail.com>
2020-04-28 10:50:33 +0530

    Update Find Angle MBC.py

--- Find Angle MBC.py
+++ Find Angle MBC.py
@@ -1,6 +1,6 @@
 import math
 x=float(input())
 y=float(input())
-z=math.sqrt((x*x)+(y*y))
-q=round(math.degrees(math.asin(x/z)))
-print ("%d°" %q)
+z=math.sqrt((x*x)+(y*y))                    #calculating sqrt(x^2+y^2)
+q=round(math.degrees(math.asin(x/z)))       #required angle is sine_inverse(x/z)-by geometry
+print ("%d°" %q)                            #printing the angle with the degree sign
