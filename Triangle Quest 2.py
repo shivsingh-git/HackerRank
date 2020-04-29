7d5669252b5d3925c84585eee94044de18be1ed1
Shiv Pratap Singh <pratapsinghshiv898@gmail.com>
2020-04-29 20:30:35 +0530

    Update Triangle Quest 2.py

--- Triangle Quest 2.py
+++ Triangle Quest 2.py
@@ -1,1 +1,5 @@
-for i in range(1,int(input())+1): print(pow(((pow(10,i)-1)//9),2))
+#getting the inputs in the for loop as the range end.
+#1^2=1,11^2=121,11^3=12321.....basic ideation
+for i in range(1,int(input())+1):                     #running the forloop from 1 to input(n)
+  print(pow(((pow(10,i)-1)//9),2))                    #frstly getting the values as 1,11,111, for respective 1,2,3..
+                                                      #then squaring them to get the required output.
