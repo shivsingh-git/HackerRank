b88f2461fd3b780ab70262f57870f5b76a58f999
Shiv Pratap Singh <pratapsinghshiv898@gmail.com>
2020-04-29 20:26:13 +0530

    Update Triangle Quest 1.py

--- Triangle Quest 1.py
+++ Triangle Quest 1.py
@@ -1,2 +1,3 @@
-for i in range(1,int(input())):
-    print(((pow(10,i)-1)//9)*i)
+#getting the input in the for loop only
+for i in range(1,int(input())):                 #running the forloop for n times
+    print(((pow(10,i)-1)//9)*i)                 #a formula to calculate 1,11,111 for respective inputs and then multioly it by the i.
