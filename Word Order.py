d70d035d0784073df05045c6aecea7748ece84b3
Shiv Pratap Singh <pratapsinghshiv898@gmail.com>
2020-06-09 21:35:42 +0530

    Update Word Order.py

--- Word Order.py
+++ Word Order.py
@@ -4,7 +4,7 @@ words = OrderedDict()
 for _ in range(int(input())):           
     word = input()                                  #getting all the inputs required
     words.setdefault(word, 0)                       #making the value for the word as 0
-    words[word] += 1                                #increasing the word count
+    words[word] += 1                                #increasing the word count.
    
 print(len(words))                                   #printing the lenght of the dictionary   
 print(*words.values())                              #printing every single words by value
