9d9be13f137051621aed3941a51ac6a9f9394a10
Shiv Pratap Singh <pratapsinghshiv898@gmail.com>
2020-06-15 08:43:19 +0530

    Create FIZZBUZZ.py

---
+++ FIZZBUZZ.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'fizzBuzz' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def fizzBuzz(n):
+    for x in range(1,n+1):
+     if x%15==0:print("FizzBuzz")
+     elif x%3==0:print("Fizz")
+     elif x%5==0:print("Buzz")
+     else:print(x)
+if __name__ == '__main__':
