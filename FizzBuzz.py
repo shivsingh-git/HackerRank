4d1d20542d330660cca807368977c49a59963279
Shiv Pratap Singh <pratapsinghshiv898@gmail.com>
2020-06-15 09:15:04 +0530

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,18 +1,11 @@
 #!/bin/python3
-
 import math
 import os
 import random
 import re
 import sys
-
-
-#
 # Complete the 'fizzBuzz' function below.
-#
 # The function accepts INTEGER n as parameter.
-#
-
 def fizzBuzz(n):
     for x in range(1,n+1):
      if x%15==0:print("FizzBuzz")
